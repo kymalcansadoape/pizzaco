a6e9720ab53fba8e357205fe6402e53de3d6a8e0
Kym Alcansado Ape <kymalcansadoape@gmail.com>
2022-07-13 01:06:19 +0800

    first commit

---
+++ resources/views/main/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-3xl text-blue-500">NI GANA HAHAHA</h1>
+</body>
+</html>
